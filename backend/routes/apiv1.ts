5c6ef8b0b0cca97aceb5619ef896df8f513bf03e
Martin Rubi <martinrubi@gmail.com>
2019-07-14 15:35:44 -0300

    Inicio del ejercicio.

---
+++ backend/routes/apiv1.ts
@@ -0,0 +1,17 @@
+import * as express from 'express'
+import * as asyncHandler from 'express-async-handler'
+
+import {ListUnits} from './apiv1/ListUnits'
+import {ListProducts} from './apiv1/ListProducts'
+import {CreateProductsOrder} from './apiv1/CreateProductsOrder'
+
+const router = express.Router()
+
+router.get('/units', ListUnits)
+
+router.get('/products', ListProducts)
+
+router.post('/shopping-cart/create-order', CreateProductsOrder)
+
+
+export {router}
